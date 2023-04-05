790e16054a03cc01e0a3bbd1b8107ba8ea9cebea
edelmanjm <edelmanjm@users.noreply.github.com>
2023-04-05 17:32:51 -0400

    Scaling, opacity

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,17 @@ import * as L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 function main(): void {
 
-  let map = L.map("map").setView([42.34953, -71.07844], 16);
+  let map = L.map("map").setView([42.34953, -71.07844], 18);
   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-  }).addTo(map);
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    opacity: 0.5,
+  }).addTo(map).setOpacity(0.75);
 
   L.tileLayer('https://s3.us-east-2.wasabisys.com/urbanatlases/39999059011864/tiles/{z}/{x}/{y}.png', {
     maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    opacity: 0.5,
   }).addTo(map);
 
   L.imageOverlay('http://localhost:1234/map.svg',
